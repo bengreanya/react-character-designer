a09f039188a2f8cba36f30bb4a6384dc6504c74c
Ben Greanya <bengreanya@gmail.com>
2023-01-11 17:29:21 -0800

    added character images to public

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Trying to learn react
         </a>
       </header>
     </div>
