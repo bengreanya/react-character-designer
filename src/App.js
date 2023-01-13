8361e199273a1872689cbb89f741d6ee1e729389
Ben Greanya <bengreanya@gmail.com>
2023-01-13 15:40:17 -0800

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './services/Components/Main/Main.js';
-
+import Header from './services/Components/Header/Header.js';
 function App() {
-  return <Main />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
